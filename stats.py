135dd2b746f5c7c1279ec64175f11e3d59cb1dd6
Slimickle <slimickledev@gmail.com>
2025-11-01 12:52:35 +0100

    modified function count_character to start with an empty dictionary and add characters, if they are seen for the first time

--- stats.py
+++ stats.py
@@ -9,41 +9,12 @@ def get_num_words(filepath):
 
 def count_characters(filepath):
     text = get_book_text(filepath)
-    character_count = {
-        "a": 0,
-        "b": 0,
-        "c": 0,
-        "d": 0,
-        "e": 0,
-        "f": 0,
-        "g": 0,
-        "h": 0,
-        "i": 0,
-        "j": 0,
-        "k": 0,
-        "l": 0,
-        "m": 0,
-        "n": 0,
-        "o": 0,
-        "p": 0,
-        "q": 0,
-        "r": 0,
-        "s": 0,
-        "t": 0,
-        "u": 0,
-        "v": 0,
-        "w": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0,
-        " ": 0,
-        ".": 0,
-        ",": 0,
-        "!": 0,
-        "?": 0,
-    }
+    character_count = {}
+    
     for char in text:
-        if char.lower() in character_count:
+        if char.lower() not in character_count:
+            character_count.update({char.lower(): 1})
+        else:
             character_count[char.lower()] += 1
     return character_count
 
